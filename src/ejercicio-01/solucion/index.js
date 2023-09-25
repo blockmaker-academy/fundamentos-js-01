acc7e980feacdfc5329bd8a6e7f1254b00c2e0f7
alexmf91 <alexmunoz91@gmail.com>
2023-09-25 10:58:37 +0200

    Actualizar declaracion variables

--- src/ejercicio-01/solucion/index.js
+++ src/ejercicio-01/solucion/index.js
@@ -1,31 +1,31 @@
 // Ejercicio 1: Variables y Tipos de Datos
 
 // 1. Declara una variable llamada `nombre` y asígnale tu nombre.
-let nombre = 'Juan'
+const nombre = 'Juan'
 
 // 2. Crea una variable `edad` y asígnale tu edad actual.
-let edad = 25
+const edad = 25
 
 // 3. Declara una variable `esEstudiante` y asígnale `true` si eres estudiante o `false` si no lo eres.
-let esEstudiante = true
+const esEstudiante = true
 
 // 4. Crea una variable `mensaje` que contenga un saludo utilizando las variables `nombre` y `edad`.
-let mensaje = `Hola, mi nombre es ${nombre} y tengo ${edad} años. Soy estudiante: ${esEstudiante}.`
+const mensaje = `Hola, mi nombre es ${nombre} y tengo ${edad} años. Soy estudiante: ${esEstudiante}.`
 
 // 5. Crea una variable `num1` y asígnale un número cualquiera.
-let num1 = 10
+const num1 = 10
 
 // 6. Crea otra variable `num2` y asígnale otro número.
-let num2 = 5
+const num2 = 5
 
 // 7. Realiza operaciones aritméticas.
-let suma = num1 + num2
-let resta = num1 - num2
-let multiplicacion = num1 * num2
-let division = num1 / num2
+const suma = num1 + num2
+const resta = num1 - num2
+const multiplicacion = num1 * num2
+const division = num1 / num2
 
 // 8. Crea una cadena de texto llamada `frase`.
-let frase = `Hola, mi nombre es ${nombre} y tengo ${edad} años. Soy estudiante: ${esEstudiante}.`
+const frase = `Hola, mi nombre es ${nombre} y tengo ${edad} años. Soy estudiante: ${esEstudiante}.`
 
 console.log(mensaje)
 console.log('Suma:', suma)
