d8d82aef7ee3957f7d53cf42a19c253f51ed0fb3
alexmf91 <alexmunoz91@gmail.com>
2023-10-23 20:40:45 +0200

    Añadir ejercicio 01

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,12 @@
+const nombre = 'Alex'
+
+const edad = 30
+
+const esEstudiante = true
+
+const esMayorDeEdad = edad >= 18
+const es_mayor_de_edad = edad >= 18
+
+const mensaje = 'Hola, mi nombre es ' + nombre + ' y tengo ' + edad + ' años. Soy estudiante: ' + esEstudiante + '.'
+
+console.log('Mensaje:', mensaje)
