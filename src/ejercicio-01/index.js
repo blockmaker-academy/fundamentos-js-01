a403ae19de987b4cf9aae7af52b734f835710174
isabelpsanch <isabelparraehs@gmail.com>
2023-10-23 20:39:31 +0200

    añadir ejercicos clase

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,9 @@
+const nombre = "Alex"
+const edad = 44
+const estudiante = true
+
+const esMayorDeEdad = edad 
+const mensaje = 'Hola, mi nombre es\n' + nombre' +' y tengo '+ edad + 'años.
+Soy estudiantes: '+ estudiante + '.''
+
+console.log(mensaje,)
