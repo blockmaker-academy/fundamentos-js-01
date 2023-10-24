f88a5a25967184e95034195b835676d79783867c
isabelpsanch <isabelparraehs@gmail.com>
2023-10-24 12:24:05 +0200

    Añadir ejercicios finalizados. Ejercico 1

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,9 +1,34 @@
-const nombre = "Alex"
-const edad = 44
-const estudiante = true
 
-const esMayorDeEdad = edad 
-const mensaje = 'Hola, mi nombre es\n' + nombre' +' y tengo '+ edad + 'años.
-Soy estudiantes: '+ estudiante + '.''
+// Ejercico 1
+const nombre = 'Isabel'
 
-console.log(mensaje,)
+//Ejercicio 2
+const edad = 51
+
+//Ejercicio 3
+const esEstudiante = true
+
+// Ejercicio 4
+
+const mensaje = `Hola, mi nombre es $(nombre) y tengo $(edad) años. Soy estuidante: $(esEstuduante).`
+
+// Ejercico 5
+const num1 = 10
+
+//Ejercicio 6
+const num2 = 5
+
+// Ejercicio 7
+const suma = num1 + num2
+const resta = num1 - num2
+const multiplicacion = num1 * num2
+const division = num1 / num2
+// Ejercico 8
+
+const frase = `Hola, mi nombre es $(nombre) y tengo $(edad) años. Soy estudiante: $(esEstudiante)`
+
+console.log(mensaje)
+console.log('Suma:', suma)
+console.log('Resta:', resta)
+console.log('Multiplicación', multiplicacion)
+console.log(frase)
