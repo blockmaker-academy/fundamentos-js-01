3b28cc686e3de38ab2d0c53b3dec5fb588a65b35
Ervigio <ejadd_75@hotmail.com>
2023-10-24 19:36:54 +0100

    ejercicio-01 resuelto satisfactoriamente

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,26 @@
+const nombre = "Ervigio"
+
+const edad = 48
+
+const esEstudiante = true
+
+const mensaje = "Hola, mi nombre es " + nombre + " y tengo " + edad + " años. Soy estudiante: " + esEstudiante + "."
+
+const num1 = 7
+const num2 = 9
+
+const suma = num1 + num2
+const resta = num1 - num2
+
+const multiplicacion = num1 * num2
+const division = num1 / num2
+const frase = `Hola, mi nombre es ${nombre} tengo ${edad} años. Soy estudiante: ${esEstudiante}.`
+
+
+console.log("Mensaje: " , mensaje)
+console.log("Suma: " , suma)
+console.log("Resta: " , resta)
+console.log("Multiplicación: " , multiplicacion)
+console.log("División: " , division)
+console.log("Frase: " , frase)
+
