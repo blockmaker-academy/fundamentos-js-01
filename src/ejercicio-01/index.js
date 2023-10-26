290a7c4750744b6e6f7566cf52f750b3fab7d81e
Puri <purificacion.bastida@gmail.com>
2023-10-26 22:47:24 +0200

    Añadir ejercicio 1

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,8 @@
+const nombre = 'Puri' //solo las cadenas de texto llevan comillas //
+const edad = 47
+const esEstudiante = true
+const esMayorDeEdad = edad >= 18
+
+const mensaje = 'Hola, mi nombe es ' + nombre + ' y tengo ' + edad + 'años. Soy estudiante: ' + esEstudiante + '.'
+
+console.log('Mensaje:', mensaje)
