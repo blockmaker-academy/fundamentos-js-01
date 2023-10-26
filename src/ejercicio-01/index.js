062c6972c003eb55b87d8c3e498bbd51956897e7
johannymk7 <johannymk7@github.com>
2023-10-26 20:04:46 +0200

    modified:   src/ejercicio-01/index.js

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,12 @@
+const nombre ='Joha'
+
+const edad = 45
+
+const esEstudiante =true
+
+const esMayorDeEddad = edad >= 18
+
+
+const mensaje = 'Hola, mi nombre es ' + nombre + ' y tengo ' + edad + ' años. Soy estudiante: ' + esEstudiante + '.';
+
+console.log('Mensaje: ' , mensaje)
