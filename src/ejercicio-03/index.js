a7ff24b7c2780a615eb6f9445432bc2dc9a0827e
Ervigio <ejadd_75@hotmail.com>
2023-10-26 23:18:00 +0100

    Aprobar ejercicio-03

---
+++ src/ejercicio-03/index.js
@@ -0,0 +1,13 @@
+// 1.
+let numero = 17
+
+//2.
+let tipoNumero = (numero>0) ? `El número ${numero} es positivo` : `El número ${numero} es cero o negativo`;
+console.log(tipoNumero)
+
+//3.
+let cadena = "Hola, éste es el texto"
+
+//4.
+let longitudCadena = cadena.length > 5 ? `La cadena de texto "${cadena}", es larga` : `La cadena: "${cadena}", es corta`;
+console.log(longitudCadena)
