ef38a4187190bcdb01f785978a347aa00cbf3f17
Elizabeth-05 <ramirezelizabeth05~@~gmail.com>
2023-10-26 18:09:23 +0200

    añadir ejercicio 1

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,10 @@
+const nombre = 'Elizabeth'
+
+const edad = 32
+
+const esEstudiante = true
+
+const mensaje = 'Hola, mi nombre es ' + nombre + ' y tengo ' + edad + ' años.
+Soy estudiante: ' + esEstudiante + ' , '
+
+console.log('mensaje:', mensaje')
