4b3e1826ab783b06cc84cc0fd9ea57344aa4e450
johannymk7 <johannymk7@github.com>
2023-10-26 21:41:53 +0200

    Añadir modifi:index.js/ejercicio1

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -10,3 +10,19 @@ const esMayorDeEddad = edad >= 18
 const mensaje = 'Hola, mi nombre es ' + nombre + ' y tengo ' + edad + ' años. Soy estudiante: ' + esEstudiante + '.';
 
 console.log('Mensaje: ' , mensaje)
+
+const num1 = 10
+
+const num2 = 5
+
+const suma = num1 + num2
+const resta = num1 - num2
+const multiplicacion = num1 * num2
+const divicion = num1 / num2
+
+console.log(mensaje)
+console.log('Suma: ' , suma)
+console.log('Resta: ', resta)
+console.log('Multiplicación: ', multiplicacion)
+console.log('División: ', divicion)
+
