aafad118f243e1017e156d412c174ae22dc42fe2
johannymk7 <johannymk7@github.com>
2023-10-27 11:18:17 +0200

    Añadir cambios index.js/ejercicio3

---
+++ src/ejercicio-03/index.js
@@ -0,0 +1,15 @@
+//  Operador Ternario y Tipos de datos
+
+// Variable Numero
+const numero =15
+
+// Operador ternario para asignar 'tipoNumero'
+const tipoNumero = numero > 0 ? 'positivo' : 'negativo o cero'
+console.log('El número es ' + tipoNumero)
+
+// Variable 'cadena' 
+const cadena = 'Hola Mundo'
+
+// Operador trnario para asignar 'longitudcadena'
+const longitudCadena = cadena.length > 5 ? 'larga' : 'corta'
+console.log('La cadena es ' + longitudCadena)
