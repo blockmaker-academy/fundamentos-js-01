ed467e8891c61b7b6a04b432687dc9b98d73d62a
Ervigio <ejadd_75@hotmail.com>
2023-10-29 16:47:22 +0000

    Resolver puntos 4, 5 y 6

--- src/ejercicio-04/index.js
+++ src/ejercicio-04/index.js
@@ -11,10 +11,30 @@
 }
 
 // 3. Función MayorDeEdad
-
 function esMayorDeEdad(edad) {
     return edad === 18 || edad >18;
 }
 const edadUsuario = 17;
 console.log(`El usuario con edad ${edadUsuario} es mayor de edad: ` , esMayorDeEdad(edadUsuario))
 
+// 4. Mayor de edad y Estudiante
+let edadPersona = 23
+let esEstudiante = false
+
+if (esMayorDeEdad(edadPersona) && esEstudiante) {
+    console.log(`La Persona con ${edadPersona} es mayor de edad: `, esMayorDeEdad(edadPersona), `, Además esta persona tambén es estudiante: `, esEstudiante);
+} else {
+    console.log(`Esta persona no cumple ambos requisitos: ser mayor de edad y estudiante`);
+}
+
+// 5. Función esVocal
+function esVocal(letra) {
+    let vocales = "aeiouAEIOU"
+    return vocales.includes(letra)
+}
+
+// 6. Usar función esVocal
+let letra = "b"
+let vocal = "U"
+console.log(`la letra ${letra} es vocal: `, esVocal(letra))
+console.log(`la letra ${vocal} es vocal: `, esVocal(vocal))
