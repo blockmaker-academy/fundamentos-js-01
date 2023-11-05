a735052bc7e395d16e37855285b6f2ac265dd4a8
Puri <purificacion.bastida@gmail.com>
2023-11-05 22:00:50 +0100

    Añadir ejercicio 3

---
+++ src/ejercicio-03/index.js
@@ -0,0 +1,15 @@
+// Variable número
+const numero = 15
+
+// Operador ternario para asignar tipoNumero
+const tipoNumero = numero > 0 ? 'positivo' : 'negativo o cero'
+console.log('El número es ' + tipoNumero)
+
+// Variable cadena
+const cadena = 'Hola Mundo'
+
+// Operador ternario para asignar longitudCadena
+const longitudCadena = cadena.length > 5 ? 'larga' : 'corta'
+console.log ('La cadena es ' + longitudCadena)
+
+
