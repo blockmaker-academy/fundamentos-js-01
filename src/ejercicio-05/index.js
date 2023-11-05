0b8b61603f1526cfaf7bcc01ebefdfa017ec296d
Puri <purificacion.bastida@gmail.com>
2023-11-05 22:49:35 +0100

    Añadir ejercicio 5

---
+++ src/ejercicio-05/index.js
@@ -0,0 +1,46 @@
+// Variable mes
+const mes = 7
+
+// Switch para estaciones del año
+switch (mes) {
+    case 12: 
+    case 1:
+    case 2:
+        console.log('Invierno')
+        break
+    case 3:
+    case 4:
+    case 5:
+        console.log('Primavera')
+        break
+    case 6:
+    case 7:
+    case 8:
+        console.log('Verano')
+        break
+    case 9:
+    case 10:
+    case 11:
+        console.log('Otoño')
+        break 
+    default:
+        console.log('Mes inválido')
+
+// Función calcularDiasMes
+function calcularDiasMes(mes) {
+    switch (mes) {
+        case 2:
+            return 28
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return 30
+        default:
+            return 31
+    }
+}
+
+// Utiliza la función calcularDiasMes
+const mesEspecifico = 4
+console.log('El mes ' + mesEspecifico + 'tiene ' + calcularDiasMes(mesEspecifico) + ' días.')
